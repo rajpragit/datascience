e8965a0cd67b6f11824f9ae5893333af2bc68885
rajpragit <raj2573@yahoo.com>
2017-06-21 19:56:36 -0700

    bank data analysis

--- springboard/project/capstone.R
+++ springboard/project/capstone.R
@@ -2,6 +2,8 @@ library(readxl)
 library(dplyr)
 library(ggplot2)
 library(ggmap)
+library(caTools)
+library(ROCR)
 
 #rm(bankdata)
 bankdata <- read_excel("C:/DataScience/code/bank.xls")
@@ -227,3 +229,38 @@ balance_30000 <- bankdata[bankdata$balance < 30000, ]
 ggplot(balance_30000, aes(y=balance, x=duration_minutes,col=factor(signup))) +
   geom_point() +
   geom_jitter() 
+
+  
+#---------------------------------------------------------------------------------------
+# Logical regression
+#
+
+t_signup <-  table(bankdata$signup)
+t_signup[1]/(t_signup[1]+t_signup[2])
+
+set.seed(22)
+split=sample.split(bankdata$signup, SplitRatio = 0.12)
+split
+bankdataTrain=subset(bankdata, split==TRUE)
+bankdataTest=subset(bankdata, split==FALSE)
+View(bankdataTrain)
+View(bankdataTest)
+nrow(bankdataTrain) 
+
+bankdataLog = glm(signup ~ age + balance , data=bankdataTrain, family=binomial)
+summary(bankdataLog)
+predictTrain=predict(bankdataLog, type="response")
+summary(predictTrain)
+tapply(predictTrain, bankdataTrain$signup, mean)
+
+table(bankdataTrain$signup, predictTrain > 0.5)
+
+ROCRpred = prediction(predictTrain,bankdataTrain$signup)
+ROCRperf = performance(ROCRpred, "tpr", "fpr")
+plot(ROCRperf)
+plot(ROCRperf, colorize=TRUE)
+plot(ROCRperf, colorize=TRUE, print.cutoffs.at=seq(0,1,0.1), text.adj=c(-0.2,1.7))
+
+predictTest=predict(bankdataLog, type="response", newdata=bankdataTest)
+
+table(bankdataTest$signup, predictTest > 0.5)
